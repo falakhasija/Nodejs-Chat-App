59acecdb4b2c23b7b84b25c1bcad4e38700e3852
Anshul Bhardwaj <anshulbhardwaj798@gmail.com>
2021-06-30 00:31:10 +0530

    Buttons working

--- app.js
+++ app.js
@@ -30,7 +30,14 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
         socket.to(roomId).broadcast.emit('user-connected', userId);
-    })
-})
 
-server.listen(3030);
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message);
+        });
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
+        })
+    });
+});
+
+server.listen(process.env.PORT||3030);
